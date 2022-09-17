a45cb0a79da1cf6e0ec8db83519a422a541aa68c
JustinGebing <justingebing@gmail.com>
2022-09-17 13:41:08 -0400

    Project folder with database and account.py and accounts.py setup

---
+++ Group_10_Project/flask_app/controllers/accounts.py
@@ -0,0 +1,60 @@
+from flask_app import app
+from flask import render_template, redirect, request, flash, session
+from flask_app.models.account import Account
+from flask_bcrypt import Bcrypt
+bcrypt=Bcrypt(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/process', methods=["POST"])
+def process():
+    if not Account.validate_account(request.form):
+        return redirect('/')
+
+    pw_hash = bcrypt.generate_password_hash(request.form['password'])
+    print(pw_hash)
+
+    data = {
+        'first_name': request.form['first_name'],
+        'last_name' : request.form['last_name'],
+        'email' : request.form['email'],
+        'password': pw_hash,
+        }
+    
+    account = Account.save(data)
+    session['id'] = account
+    flash(f'{request.form["email"]}, you have registered!')
+    return redirect('/dashboard')
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    Account.delete(
+        {'id': id}
+    )
+    return redirect('/')
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = {
+        'email' : request.form['email'],
+        }
+
+    account = Account.get_one_email(data)
+    if account:
+        if not bcrypt.check_password_hash(account.password, request.form['password']):
+            flash('Email or Password is incorect!')
+            return redirect('/')
+    
+        session['id'] = account.id
+        flash(f'{account.first_name}, you have successfully logged in!')
+        return redirect('/dashboard')
+
+    flash('Email not recognized')
+    return redirect('/')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
