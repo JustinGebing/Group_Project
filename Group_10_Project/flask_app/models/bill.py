6d0cfde305c86b22b363be76b72e5d86b11a2d73
Scott-Nelson9076 <98765473+Scott-Nelson9076@users.noreply.github.com>
2022-09-20 15:43:55 -0600

    Data Rename Commit 3

--- Group_10_Project/flask_app/models/bill.py
+++ Group_10_Project/flask_app/models/bill.py
@@ -5,7 +5,7 @@ from flask import flash
 class Bill:
     def __init__(self,data) -> None:
         self.id = data['id']
-        self.bill_name = data['bill_name']
+        self.name = data['name']
         self.image = data['image']
         self.due_date = data['due_date']
         self.amount = data['amount']
@@ -16,7 +16,7 @@ class Bill:
     @staticmethod
     def validatebill(bill):
         is_valid = True
-        if len(bill['bill_name']) < 2:
+        if len(bill['name']) < 2:
             flash("Bill Name Is A Required Field And Must Be More than 2 Characters")
             is_valid = False
         if len(bill['due_date']) < 1:
@@ -28,12 +28,12 @@ class Bill:
         return is_valid
     @classmethod
     def addbill(cls,data):
-        query = "INSERT INTO bills (bill_name,image,due_date,amount,recurring,account_id ) VALUES (%(bill_name)s, %(image)s, %(due_date)s, %(amount)s, %(recurring)s, NOW(), NOW(), %(account_id)s)"
+        query = "INSERT INTO bills (name,image,due_date,amount,recurring,account_id ) VALUES (%(name)s, %(image)s, %(due_date)s, %(amount)s, %(recurring)s, NOW(), NOW(), %(account_id)s)"
         return connectToMySQL('Bills-n-Stuff').query_db(query,data)
 
     @classmethod
     def updatebill(clas,data):
-        query = "UPDATE bills SET bill_name = %(bill_name)s, image = %(image)s, due_date = %(due_date)s, how_much = %(amount)s, recurring = %(recurring)s, updated_at = NOW() WHERE id = %(id)s"
+        query = "UPDATE bills SET name = %(name)s, image = %(image)s, due_date = %(due_date)s, how_much = %(amount)s, recurring = %(recurring)s, updated_at = NOW() WHERE id = %(id)s"
         return connectToMySQL('Bills-n-Stuff').query_db(query,data)
 
     @classmethod
