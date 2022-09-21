3076bb13f52d69b088617787d7ea68163985dd9a
Jonathan Jones <97317524+JonJones98@users.noreply.github.com>
2022-09-21 02:57:53 -0400

    Adding Image to text parser

--- Group_10_Project/flask_app/controllers/bills.py
+++ Group_10_Project/flask_app/controllers/bills.py
@@ -2,6 +2,7 @@ from flask import render_template, redirect, session, request, flash
 from flask_app import app
 from flask_app.models.bill import Bill
 from flask_app.models.account import Account
+from flask_app.models.image_text import Image_text
 
 #Route to create bill page
 @app.route('/new/bill')
