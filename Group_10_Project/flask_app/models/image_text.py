244ee58e3bc23993c896b2653725cf3962719bbf
Jonathan Jones <97317524+JonJones98@users.noreply.github.com>
2022-09-21 22:35:37 -0400

    Updated image_text.py

--- Group_10_Project/flask_app/models/image_text.py
+++ Group_10_Project/flask_app/models/image_text.py
@@ -14,58 +14,63 @@ class Image_text:
         #Save image to temporary location
         UPLOAD_FOLDER = 'flask_app/static/img/'+filename
         app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-        image1 = Image.open(image)
-        image1.save(UPLOAD_FOLDER)
-        #Define path to image
-        path_to_image = UPLOAD_FOLDER
-        #Open image with PIL
-        img = Image.open(path_to_image)
-        
-        #Extract text from image
-        text = pytesseract.image_to_string(img)
-        print(text)
+        try:
+            image1 = Image.open(image)
+            image1.save(UPLOAD_FOLDER)
+            #Define path to image
+            path_to_image = UPLOAD_FOLDER
+            #Open image with PIL
+            img = Image.open(path_to_image)
+            
+            #Extract text from image
+            text = pytesseract.image_to_string(img)
+            print(text)
 
-        #Preparing Text data
-        part1=(text.replace("\n"," ")).upper()
-        part2=part1.split(" ")
-        part3=(text.upper()).split("\n")
-        price=""
+            #Preparing Text data
+            part1=(text.replace("\n"," ")).upper()
+            part2=part1.split(" ")
+            part3=(text.upper()).split("\n")
+            price=""
 
-        #Finding Total from image
-        #Removing blanks values in list
-        for a in part3:
-            if a=='':
-                part3.remove(a)
-        #Splitting each list attribute int
-        for b in part3:
-                c=b.split(" ")
-                x=-1
-                #Finding 
-                print (c)
-                for d in c:
-                    x+=1
-                    if "SUB" in c:
-                        continue
-                    elif "TAX" in d:
-                        continue
-                    elif "TOTAL" or ("USD"and"TOTAL") in d:
-                        for e in range(len(c)):
-                            f=c[e].replace(",",".")
-                            try:
-                                if float(f)>0:
-                                    price = f
-                                    break
-                            except:
-                                g=f.replace("$","")
+            #Finding Total from image
+            #Removing blanks values in list
+            for a in part3:
+                if a=='':
+                    part3.remove(a)
+            #Splitting each list attribute int
+            for b in part3:
+                    c=b.split(" ")
+                    x=-1
+                    #Finding 
+                    for d in c:
+                        x+=1
+                        if "SUB" in c or d:
+                            continue
+                        elif "TAX" in d:
+                            continue
+                        elif "TOTAL"in d:
+                            for e in range(len(c)):
+                                f=c[e].replace(",",".")
+                                
                                 try:
-                                    if float(g)>0:
-                                        price = g
+                                    if float(f)>0:
+                                        price = f
+                                        print (f)
                                         break
+                                        
                                 except:
-                                    continue
-        if price=="":
-            total="error"
-            print("Image unclear.Resubmit more clear image")
-        total=price
-        os.remove(UPLOAD_FOLDER)
+                                    g=f.replace("$","")
+                                    try:
+                                        if float(g)>0:
+                                            price = g
+                                            break
+                                    except:
+                                        continue
+            if price=="":
+                total="Image unclear. Re-upload new image"
+            else:
+                total=price
+            os.remove(UPLOAD_FOLDER)
+        except:
+            total="Error with image"
         return total
